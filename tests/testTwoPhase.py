76f1e37a67feab9123f82031b60e388aae4cc3c8
yagizpancak <yagizcan923@gmail.com>
2024-05-09 17:40:07 +0300

    add test

---
+++ tests/testTwoPhase.py
@@ -0,0 +1,41 @@
+from adhoccomputing.Generics import *
+from adhoccomputing.Experimentation.Topology import Topology
+from CommitProtocols.TwoPhaseCommit import *
+import time
+
+class Node(GenericModel):
+    def on_init(self, eventobj: Event):
+        self.Coordinator.on_commit()
+
+
+    def __init__(self, componentname, componentinstancenumber, context=None, configurationparameters=None,
+                 num_worker_threads=1, topology=None):
+        super().__init__(componentname, componentinstancenumber, context, configurationparameters, num_worker_threads,
+                         topology)
+        # SUBCOMPONENTS
+        self.Coordinator = TwoPhaseCommitCoordinator("Coordinator", componentinstancenumber)
+        self.P1 = TwoPhaseCommitParticipant("Participant 1", componentinstancenumber)
+        self.P2 = TwoPhaseCommitParticipant("Participant 2", componentinstancenumber)
+
+        self.components.append(self.Coordinator)
+        self.components.append(self.P1)
+        self.components.append(self.P2)
+
+        ## CONNECTION using U (up) D (down) P (peer) functions
+        self.Coordinator.D(self.P1)
+        self.Coordinator.D(self.P2)
+
+        self.P1.U(self.Coordinator)
+        self.P2.U(self.Coordinator)
+
+
+def main():
+  #setAHCLogLevel(DEBUG)
+  topo = Topology();
+  topo.construct_single_node(Node, 0)
+  topo.start()
+  time.sleep(1)
+  topo.exit()
+
+if __name__ == "__main__":
+  main()
